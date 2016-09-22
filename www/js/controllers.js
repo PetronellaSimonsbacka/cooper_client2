3d2eb6060d338773261a56057669991a7071dea9
PetronellaSimonsbacka <gussimonpe@student.gu.se>
2016-09-22 14:57:47 +0200

    Added log in controller

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,13 +1,14 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('AppCtrl', function($scope, $ionicModal, $timeout, $auth) {
 
   $scope.loginData = {};
 
   $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
+      scope: $scope
+    })
+    .then(function(modal) {
+      $scope.modal = modal;
   });
 
   $scope.closeLogin = function() {
@@ -20,6 +21,15 @@ angular.module('starter.controllers', [])
 
   $scope.doLogin = function() {
     console.log('Doing login', $scope.loginData);
+    $auth.submitLogin($scope.loginData)
+      .then(function (resp) {
+            // handle success response
+            $scope.closeLogin();
+      })
+      .catch(function (error) {
+            // handle error response
+      $scope.errorMessage = error;
+      });
 
     $timeout(function() {
       $scope.closeLogin();
@@ -34,20 +44,22 @@ angular.module('starter.controllers', [])
     max: 60,
     value: 20
   };
+
   $scope.distanceValues = {
     min: 1000,
     max: 3500,
     value: 1000
   };
-  
+
   $scope.data = {};
   $scope.calculateCooper = function() {
     var person = new Person({
       gender: $scope.data.gender,
       age: $scope.data.age
     });
+
     person.assessCooper($scope.data.distance);
     $scope.person = person;
     console.log($scope.person)
-    }
+    };
   });
